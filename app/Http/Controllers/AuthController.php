485b9fdeaae25a614f59c07b5bb6070fe511fb67
Jose Quispe <jkqr2016@gmail.com>
2024-08-10 18:10:31 -0500

    Autenticando al usuario

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -37,6 +37,13 @@ class AuthController extends Controller
                 'errors' => ['El email o el password son incorrectos']
             ], 422);
         }
+
+        // Autenticar al usuario
+        $user = Auth::user();
+        return [
+            'token' => $user->createToken('token')->plainTextToken,
+            'user' => $user
+        ];
     }
 
     public function logout(Request $request) {
