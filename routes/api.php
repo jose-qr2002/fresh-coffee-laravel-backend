585826ed89a323a6c2902abdffdaa2a918850016
Jose Quispe <jkqr2016@gmail.com>
2024-08-10 17:14:09 -0500

    Agregando validaciones al Login

--- routes/api.php
+++ routes/api.php
@@ -15,3 +15,4 @@ Route::apiResource('/productos', ProductoController::class);
 
 // Autenticacion
 Route::post('/registro', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
