18cb18e32d41f2e79d60243998c673ee8d7afe78
Jose Quispe <jkqr2016@gmail.com>
2024-08-11 16:54:38 -0500

    Implementando logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -47,6 +47,10 @@ class AuthController extends Controller
     }
 
     public function logout(Request $request) {
-
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+        return [
+            'user' => null
+        ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
 Route::apiResource('/categorias', CategoriaController::class);
 Route::apiResource('/productos', ProductoController::class);
 
